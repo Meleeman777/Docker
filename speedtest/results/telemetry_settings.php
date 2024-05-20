f7bf363944381dfe4aab7dca804340f3b21bb55c
Ivan Abramenko <meleemanhs@gmail.com>
2024-05-20 19:07:16 +0200

    added speedtest

---
+++ speedtest/results/telemetry_settings.php
@@ -0,0 +1,15 @@
+<?php
+
+$db_type="postgresql"; //Type of db: "mysql", "sqlite" or "postgresql"
+$stats_password="speedtest"; //password to login to stats.php. Change this!!!
+$enable_id_obfuscation=true; //if set to true, test IDs will be obfuscated to prevent users from guessing URLs of other tests
+$redact_ip_addresses=false; //if set to true, IP addresses will be redacted from IP and ISP info fields, as well as the log
+// Postgresql settings
+$PostgreSql_username="speedtest";
+$PostgreSql_password="speedtest";
+$PostgreSql_hostname="postgres";
+$PostgreSql_databasename="speedtest";
+
+//IMPORTANT: DO NOT ADD ANYTHING BELOW THIS PHP CLOSING TAG, NOT EVEN EMPTY LINES!
+?>
+
